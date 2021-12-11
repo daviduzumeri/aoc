cc1b2f1c90759710c3de7a6c33c8226722eeed5e
David Uzumeri <uzumerid@gmail.com>
2021-12-11 14:02:17 -0500

    Breaking line

--- day3/main.go
+++ day3/main.go
@@ -54,7 +54,8 @@ func findNumber(numbers []int, moreCommon bool, numDigits int) int {
 	maxBit := int(math.Pow(2, float64(numDigits-1)))
 
 	for _, number := range numbers {
-		if (int(returnNumber)&maxBit > 0 && number&maxBit > 0) || (int(returnNumber)&maxBit == 0 && number&maxBit == 0) {
+		if (int(returnNumber)&maxBit > 0 && number&maxBit > 0) ||
+			(int(returnNumber)&maxBit == 0 && number&maxBit == 0) {
 			newArray = append(newArray, number)
 		}
 	}
